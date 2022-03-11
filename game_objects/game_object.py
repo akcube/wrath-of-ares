c1de9626ca5555fe48995acd70b12b593728409a
akcube <a.kishorekumar@students.iiit.ac.in>
2022-03-11 08:05:25 +0530

    Code for game object

---
+++ game_objects/game_object.py
@@ -0,0 +1,57 @@
+"""
+This file contains an interface with all the data and implementing all the 
+functions expected from a basic game object model.
+"""
+
+class GameObject:
+    """
+    This class acts as an interface for game objects. All game objects should 
+    inherit from this class.
+    """
+
+    def __init__(self, dim=np.array([0, 0]), pos=np.array([0., 0.]), velocity=0.,
+                 drawing=np.array([[" "]]), color=np.array([[" "]]), mhealth=0):
+        """
+        Constructor
+
+        Arguments:
+            dim ([h, w])            : The height and width of the object
+            pos ([x, y])            : The initial x and y coordinate of the object
+            velocity (float)        : The initial velocity of the object
+            drawing (2d np array)   : The ascii art of the object
+            color (2d np array)     : The color of each ascii character of the object
+            mhealth (int)           : The maximum health this object can have
+        """
+
+        self._dim = dim
+        self._pos = pos
+        self._velocity = velocity
+        self._drawing = drawing
+        self._color = color
+        self._mhealth = mhealth
+        self._health = mhealth
+        self._destroyed = False
+
+    def setDestroyed(self):
+        self._destroyed = True
+    
+    def setPos(self, mpos):
+        self._pos = mpos
+    
+    def movePos(self, mvec):
+        self._pos += mvec
+
+    def getRender(self):
+        return self._drawing, self._color
+
+    def getPos(self):
+        return self._pos
+
+    def getDim(self):
+        return self._dim
+
+    def getHealth(self):
+        return self._health
+    
+    def getDestroyed(self):
+        return self._destroyed
